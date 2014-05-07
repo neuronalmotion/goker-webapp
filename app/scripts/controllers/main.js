b7c82ae0be90433a4c146e242874f167741fe905
Robin Penea <robin.penea@gmail.com>
2014-05-07 15:43:16 +0200

    App scaffolding
    Close #1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gokerWebappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
